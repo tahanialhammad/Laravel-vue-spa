930ad90bd7437b2115ecf5dfdf3d234c3ba841b1
tahanialhammad <tahani_alhammad@yahoo.com>
2024-06-27 22:46:17 +0200

    update

--- tests/Feature/Controllers/PostController/CreateTest.php
+++ tests/Feature/Controllers/PostController/CreateTest.php
@@ -4,12 +4,12 @@ use App\Models\User;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
-// it('requires authentication', function () {
-//     get(route('posts.create'))->assertRedirect(route('login'));
-// });
+it('requires authentication', function () {
+    get(route('posts.create'))->assertRedirect(route('login'));
+});
 
-// it('returns the correct component', function () {
-//     actingAs(User::factory()->create())
-//         ->get(route('posts.create'))
-//         ->assertComponent('Posts/Create');
-// });
+it('returns the correct component', function () {
+    actingAs(User::factory()->create())
+        ->get(route('posts.create'))
+        ->assertComponent('Posts/Create');
+});
