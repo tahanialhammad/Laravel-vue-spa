266e11aa9c02fa8b5443a15d293f029c44beaef4
tahanialhammad <tahani_alhammad@yahoo.com>
2024-07-05 20:02:20 +0200

    update

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Like;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class LikeController extends Controller
 {
@@ -26,9 +30,19 @@ class LikeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, string $type, int $id)
     {
-        //
+        $likeable = $this->findLikeable($type, $id);
+      //  $this->authorize('create', [Like::class, $likeable]);
+        Gate::authorize('create', [Like::class, $likeable]);
+
+
+        $likeable->likes()->create([
+            'user_id' => $request->user()->id,
+        ]);
+        $likeable->increment('likes_count');
+
+        return back();
     }
 
     /**
@@ -61,5 +75,18 @@ class LikeController extends Controller
     public function destroy(Like $like)
     {
         //
+    }
+
+    protected function findLikeable(string $type, int $id): Model
+    {
+        /** @var class-string<Model>|null $modelName */
+        $modelName = Relation::getMorphedModel($type);
+
+        if ($modelName === null) {
+            throw new ModelNotFoundException();
+        }
+
+        $likeable = $modelName::findOrFail($id);
+        return $likeable;
     }
 }
