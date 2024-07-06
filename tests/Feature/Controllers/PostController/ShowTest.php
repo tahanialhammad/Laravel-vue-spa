27529ab6ad074af9992ff3c6a17ebd592376100f
tahanialhammad <tahani_alhammad@yahoo.com>
2024-07-06 16:36:50 +0200

    update

--- tests/Feature/Controllers/PostController/ShowTest.php
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -20,7 +20,8 @@ it('passes a post to the view', function () {
     $post->load('user', 'topic');
 
     get($post->showRoute())
-        ->assertHasResource('post', PostResource::make($post));
+    //->assertHasResource('post', PostResource::make($post));
+        ->assertHasResource('post', PostResource::make($post)->withLikePermission());
 });
 
 it('passes comments to the view', function () {
@@ -29,8 +30,12 @@ it('passes comments to the view', function () {
 
     $comments->load('user');
 
+    $expectedResource = CommentResource::collection($comments->reverse());
+    $expectedResource->collection->transform(fn (CommentResource $resource) => $resource->withLikePermission());
+
     get($post->showRoute())
-        ->assertHasPaginatedResource('comments', CommentResource::collection($comments->reverse()));
+    //->assertHasPaginatedResource('comments', CommentResource::collection($comments->reverse()));
+        ->assertHasPaginatedResource('comments', $expectedResource);
 });
 
 it('will redirect if the slug is incorrect', function (string $incorrectSlug) {
@@ -38,7 +43,7 @@ it('will redirect if the slug is incorrect', function (string $incorrectSlug) {
 
     get(route('posts.show', [$post, $incorrectSlug, 'page' => 2]))
         ->assertRedirect($post->showRoute(['page' => 2]));
-        //extra test 
+        //extra test
 })->with([
     'foo-bar',
     'hello'
