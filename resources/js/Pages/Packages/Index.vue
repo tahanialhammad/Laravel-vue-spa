cb70d48a26cf05380e9340be0cf498ab77e60d2f
tahanialhammad <tahani_alhammad@yahoo.com>
2024-07-15 23:36:05 +0200

    update

--- resources/js/Pages/Packages/Index.vue
+++ resources/js/Pages/Packages/Index.vue
@@ -17,10 +17,9 @@
 
             <div class="mx-auto max-w-7xl px-6 lg:px-8 py-6 my-6">
                 <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-
                     <Card v-for="packageItem in packageItems.data" :key="packageItem.id" class="group ">
                         <template #cardHeader>
-                            <Link :href="packageItem.routes.show" class="flex justify-items-center group px-2 py-4">
+                            <Link :href="packageItem.routes.show" class="flex items-center group px-2 py-4">
                             <img class="max-h-12 object-containt me-2"
                                 :src="`/assests/packages/${packageItem.code.toLowerCase()}.svg`" :alt="packageItem.code"
                                 width="50" height="50" />
