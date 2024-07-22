6d81b26cf1a0a8c901236019e9276248bdd38a64
tahanialhammad <tahani_alhammad@yahoo.com>
2024-07-22 22:36:49 +0200

    update packages

--- resources/js/Pages/Packages/Partials/EditPackage.vue
+++ resources/js/Pages/Packages/Partials/EditPackage.vue
@@ -7,37 +7,34 @@
         <Modal :show="showModal" @close="closeModal" maxWidth="xl" closeable>
             <template #default>
                 <div class="p-6">
-                    <h2 class="text-lg font-medium text-gray-900">Modal Title {{ packageItem.code }}</h2>
+                    <h2 class="text-lg font-medium text-gray-900">Edit Package {{ packageItem.code }}</h2>
 
                     <form @submit.prevent="editPackage" class="mt-6">
                         <div>
-                            <InputLabel for="code" v-value="packageItem.code" class="sr-only">Code</InputLabel>
-                            <TextInput id="code" class="w-full" v-model="form.code"
-                                placeholder="Give it a great title…" />
+                            <InputLabel for="code" class="sr-only">Code</InputLabel>
+                            <TextInput id="code" class="w-full" v-model="form.code" placeholder="Package code" />
                             <InputError :message="form.errors.code" class="mt-1" />
                         </div>
 
                         <div class="mt-3">
                             <InputLabel for="info" class="sr-only">Info</InputLabel>
-                            <MarkdownEditor v-model="form.info" v-value="packageItem.info" editor-class="!min-h-[120px]" />
+                            <TextArea id="info" class="w-full" v-model="form.info" placeholder="Package info" />
                             <InputError :message="form.errors.info" class="mt-1" />
                         </div>
 
-                        <div class="mt-3 flex space-x-2 justify-end	">
+                        <div class="mt-3 flex space-x-2 justify-end">
                             <SecondaryButton @click="closeModal">Close</SecondaryButton>
-                            <PrimaryButton type="submit">Create package</PrimaryButton>
+                            <PrimaryButton type="submit">Save changes</PrimaryButton>
                         </div>
                     </form>
-
                 </div>
             </template>
         </Modal>
     </div>
 </template>
 
-
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useForm } from "@inertiajs/vue3";
 import Modal from '@/Components/Modal.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
@@ -45,10 +42,8 @@ import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
 import InputError from "@/Components/InputError.vue";
 import TextArea from "@/Components/TextArea.vue";
-import MarkdownEditor from "@/Components/MarkdownEditor.vue";
 import SecondaryButton from '@/Components/SecondaryButton.vue';
-import { PencilSquareIcon, EyeIcon, TrashIcon } from '@heroicons/vue/20/solid'
-
+import { PencilSquareIcon } from '@heroicons/vue/20/solid';
 
 const props = defineProps(['packageItem']);
 
@@ -56,12 +51,14 @@ const showModal = ref(false);
 
 const openModal = () => {
     showModal.value = true;
+    // Initialize form fields with packageItem data
+    form.code = props.packageItem.code;
+    form.info = props.packageItem.info;
 };
 
 const closeModal = () => {
     showModal.value = false;
 };
-
 
 const form = useForm({
     code: "",
@@ -69,14 +66,14 @@ const form = useForm({
 });
 
 const editPackage = () => {
-form.post(route('packages.update'), {
+    form.put(route('packages.update', { id: props.packageItem.id }), {
         onFinish: (response) => {
             if (Object.keys(form.errors).length === 0) {
                 closeModal();
             }
         },
         onError: () => {
-            openModal()
+            openModal();
         },
     });
 };
