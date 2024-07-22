bd926afa19c4200b851a2e2fc2f429774a70c68d
tahanialhammad <tahani_alhammad@yahoo.com>
2024-07-22 22:06:16 +0200

    update packages

--- resources/js/Pages/Packages/Partials/DeletePackage.vue
+++ resources/js/Pages/Packages/Partials/DeletePackage.vue
@@ -13,12 +13,26 @@
 <script setup>
 import { Head, Link, router, useForm } from "@inertiajs/vue3";
 import { TrashIcon } from '@heroicons/vue/20/solid'
+import { useConfirm } from "@/Utilities/Composables/useConfirm.js";
 
 
 const props = defineProps(['packageItem']);
+const { confirmation } = useConfirm();
 
-const deleteItem = () => router.delete(route('packages.destroy', props.packageItem),{
-    preserveScroll: true,
-});
+// const deleteItem = () => router.delete(route('packages.destroy', props.packageItem),{
+//     preserveScroll: true,
+// });
 
+
+const deleteItem = async (packageItem) => {
+    if (
+        !(await confirmation("Are you sure you want to delete this comment?"))
+    ) {
+        return;
+    }
+
+    router.delete(route('packages.destroy', props.packageItem), {
+        preserveScroll: true,
+    });
+};
 </script>
