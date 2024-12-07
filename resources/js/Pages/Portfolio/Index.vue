99cee6f57eb6f9d920f87f35486a769c14d32f89
tahanialhammad <tahani_alhammad@yahoo.com>
2024-12-07 21:50:04 +0100

    update and run build

---
+++ resources/js/Pages/Portfolio/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <AppLayout>
+        <Container>
+            portfolio
+        </Container>
+    </AppLayout>
+</template>
+
+<script setup>
+import Container from '@/Components/Container.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+
+</script>
